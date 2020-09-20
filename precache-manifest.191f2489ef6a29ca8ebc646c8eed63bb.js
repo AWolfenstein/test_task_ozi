7a77f07a9ad01f770c7734868f00a3744e991b45
Aleksei <alexsupa@mail.ru>
2020-09-21 02:14:40 +0300

    Updates

---
+++ precache-manifest.191f2489ef6a29ca8ebc646c8eed63bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b70c4139546ff821819c3de97cce850",
+    "url": "/test_task_ozi/index.html"
+  },
+  {
+    "revision": "78406b823774bca0e6bd",
+    "url": "/test_task_ozi/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "41dff52af287d8006316",
+    "url": "/test_task_ozi/static/css/main.4acbb3dd.chunk.css"
+  },
+  {
+    "revision": "78406b823774bca0e6bd",
+    "url": "/test_task_ozi/static/js/2.d769066d.chunk.js"
+  },
+  {
+    "revision": "ebe46f246e12518583ab0a12ae31fd2d",
+    "url": "/test_task_ozi/static/js/2.d769066d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41dff52af287d8006316",
+    "url": "/test_task_ozi/static/js/main.a2b44260.chunk.js"
+  },
+  {
+    "revision": "081eb62a762fb7082b53",
+    "url": "/test_task_ozi/static/js/runtime-main.595b8d7b.js"
+  }
+]);
